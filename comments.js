1197fa95623c91197585b6f89bab650d2f1e4c2e
Abdülfettah Adwani <74654437+FettahAud@users.noreply.github.com>
2023-10-03 07:42:34 +0000

    copilot

--- comments.js
+++ comments.js
@@ -1,1 +1,22 @@
 // create web server
+// 1. load all the required modules
+const http = require('http');
+const fs = require('fs');
+
+// 2. create a server object
+const server = http.createServer(function(req, res) {
+    // 4. read the file contents
+    fs.readFile('comments.json', 'utf-8', function(err, data) {
+        // 5. parse the contents to JSON
+        const comments = JSON.parse(data);
+        // 6. set the response header
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        // 7. send the response
+        res.end(JSON.stringify(comments));
+    });
+});
+
+// 3. listen for any incoming requests
+server.listen(8080, '
