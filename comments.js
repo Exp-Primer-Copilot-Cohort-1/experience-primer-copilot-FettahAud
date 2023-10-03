615ddf9fc5cf734242f824ebfaf22b22523e3829
Abdülfettah Adwani <74654437+FettahAud@users.noreply.github.com>
2023-10-03 07:44:38 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -6,7 +6,8 @@ const fs = require('fs');
 // 2. create a server object
 const server = http.createServer(function(req, res) {
     // 4. read the file contents
-    fs.readFile('comments.json', 'utf-8', function(err, data) {
+    fs.readFile('comments.json', 'utf-8', function(err, data) { 
+        
         // 5. parse the contents to JSON
         const comments = JSON.parse(data);
         // 6. set the response header
